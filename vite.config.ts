7da13f82096670228ccd230c945a8f28b7f70511
jiwon0995 <jiwon0995@gmail.com>
2024-11-23 22:04:59 +0900

    [Initial] 스타일 라이브러리 설치 및 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    root: '.', // 프로젝트 루트를 명시적으로 설정
-    publicDir: 'public', // 정적 파일 폴더
-    server: {
-        port: 3000,  // 포트 변경
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
+  },
+  publicDir: "public", // 정적 파일 폴더
+  server: {
+    port: 3000, // 포트 변경
+  },
 });
