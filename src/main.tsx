cad12e2f0a2b18158abebe63f82265dbab5ea2ad
jiwon0995 <jiwon0995@gmail.com>
2024-11-26 18:41:23 +0900

    [Setting] tsconfig 설정 수정, index css import

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
 import "./styles/reset.css";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
