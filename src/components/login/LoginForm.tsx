a4a5cf9f807ce035a27b8f1c44dfd3c7c4201636
jiwon0995 <jiwon0995@gmail.com>
2024-11-26 18:42:00 +0900

    [Add] 로그인 폼 컴포넌트 생성

---
+++ src/components/login/LoginForm.tsx
@@ -0,0 +1,78 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
+
+const formSchema = z.object({
+  email: z.string().email({
+    message: "올바른 이메일 주소를 입력해주세요.",
+  }),
+  password: z.string().min(8, { message: "비밀번호는 최소 8자 이상이어야 합니다." }),
+});
+
+export default function LoginForm() {
+  const [isShowPassword, isSetShowPassword] = useState<boolean>(false);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    const password = values.password.trim();
+    console.log("Submit", values, password);
+    // todo: login api 연결
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="example@domain.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <div className="relative">
+                  <Input type={isShowPassword ? "text" : "password"} placeholder="password" {...field} />
+                  <button
+                    type="button"
+                    className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-400 hover:text-gray-600"
+                    onClick={() => isSetShowPassword(!isShowPassword)}
+                  >
+                    {isShowPassword ? <EyeOff className="h-5 w-5" /> : <Eye className="h-5 w-5" />}
+                  </button>
+                </div>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full">
+          Login
+        </Button>
+      </form>
+    </Form>
+  );
+}
