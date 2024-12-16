53bd258266d5b6dc92a9ff8118140c5ba43ba598
jiwon0995 <jiwon0995@gmail.com>
2024-12-16 23:03:04 +0900

    [Update] 로그인 페이지 라우터 이름 변경

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import LoginPage from "../pages/Login";
+import LoginPage from "../pages/SignIn";
+import HomePage from "@/pages/Home";
 
 export default function AppRouter() {
   return (
     <Routes>
-      <Route path={"/login"} element={<LoginPage />} />
+      <Route path={"/signin"} element={<LoginPage />} />
+      <Route path={"/home"} element={<HomePage />} />
     </Routes>
   );
 }
