2a7d1300286d80c5472bff2e52fd9eb774835c59
jiwon0995 <jiwon0995@gmail.com>
2024-12-17 23:42:18 +0900

    [Add] 회원가입 페이지 생성

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "../pages/SignIn";
 import HomePage from "@/pages/Home";
+import SignUpPage from "@/pages/SignUp";
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path={"/signin"} element={<LoginPage />} />
       <Route path={"/home"} element={<HomePage />} />
+      <Route path={"/signup"} element={<SignUpPage />} />
     </Routes>
   );
 }
