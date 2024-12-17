e157b426750366402f86a4189f30dad4e4cebb95
jiwon0995 <jiwon0995@gmail.com>
2024-12-17 23:42:30 +0900

    [Add] 회원가입 api 추가

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -6,3 +6,7 @@ const PREFIX_PATH = "/api";
 export const sessionApi = async (email: string, password: string): Promise<CommonResponse<string>> => {
   return await api.post(`${PREFIX_PATH}/session`, { email, password }).then((res) => res.data);
 };
+
+export const signUpApi = async (email: string, name: string, password: string): Promise<CommonResponse<string>> => {
+  return await api.post(`${PREFIX_PATH}/users`, { email, name, password }).then((res) => res.data);
+};
